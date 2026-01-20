5e9be47272b00d466778ea8de4d1f9040a9d4a02
dmlyrae <dmlyrae@users.noreply.github.com>
2026-01-20 03:07:15 +0000

    deploy: 9521571e4b5c188dbada67038738fc080c40a313

---
+++ search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['get_5fversion_0',['get_version',['../lib_8cpp.html#acd3d86931da3aac380551f64204c6a26',1,'get_version():&#160;lib.cpp'],['../lib_8h.html#acd3d86931da3aac380551f64204c6a26',1,'get_version():&#160;lib.cpp']]]
+];
---
+++ search/all_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['print_5fip_0',['print_ip',['../print__ip_8h.html#a14dc353cc0cd9f2442a94965c20a8cdf',1,'print_ip(T value):&#160;print_ip.h'],['../print__ip_8h.html#adc87c2202fc1519a6d2ff6fe1fc96a78',1,'print_ip(const T &amp;value):&#160;print_ip.h'],['../print__ip_8h.html#ada0bef2c70388ab2461101bf2a326e5a',1,'print_ip(const char *value):&#160;print_ip.h'],['../print__ip_8h.html#a5e9b6f224d60d72b1a49e454fb1b80e1',1,'print_ip(const Container&lt; T, std::allocator&lt; T &gt; &gt; &amp;container):&#160;print_ip.h'],['../print__ip_8h.html#ac939d3ddb4c5a739800a9b851fd0a271',1,'print_ip(const std::tuple&lt; TuppleTypes... &gt; &amp;tuple):&#160;print_ip.h']]],
+  ['print_5fip_2eh_1',['print_ip.h',['../print__ip_8h.html',1,'']]],
+  ['print_5ftuple_5fimpl_2',['print_tuple_impl',['../print__ip_8h.html#a2a8571c00665627a6e95d6ee29cbcbd0',1,'print_tuple_impl(const std::tuple&lt; TuppleTypes... &gt; &amp;tuple, bool &amp;first):&#160;print_ip.h'],['../print__ip_8h.html#a620d671a0f0d5aa7e8664f06328e8db3',1,'print_tuple_impl(const std::tuple&lt; TuppleTypes... &gt; &amp;tuple, bool &amp;first):&#160;print_ip.h']]]
+];
